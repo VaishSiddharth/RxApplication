06584a25c0f4cd3e1cf53948b0b37bd53a705f4f
Siddharth Vaish <sidvaish97@gmail.com>
2019-11-08 17:34:46 +0530

    test how observables work

--- app/src/main/java/com/siddharth/rxapplication/MainActivity.java
+++ app/src/main/java/com/siddharth/rxapplication/MainActivity.java
@@ -10,6 +10,7 @@ import io.reactivex.Observer;
 import io.reactivex.Scheduler;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,26 +24,38 @@ public class MainActivity extends AppCompatActivity {
         Observable<Task> taskObservable = Observable
                 .fromIterable(DataSource.createTasksList())
                 .subscribeOn(Schedulers.io())
+                .filter(new Predicate<Task>() {
+                    @Override
+                    public boolean test(Task task) throws Exception {
+                        Log.d(TAG, "test " + Thread.currentThread().getName());
+                        Thread.sleep(1000);
+
+                        return task.isComplete();
+                    }
+                })
                 .observeOn(AndroidSchedulers.mainThread());
 
         taskObservable.subscribe(new Observer<Task>() {
             @Override
             public void onSubscribe(Disposable d) {
-                Log.e(TAG,"onSubscribe called" );
+                Log.d(TAG, "onSubscribe called");
             }
 
             @Override
             public void onNext(Task task) {
-
+                Log.d(TAG, "onNext" + Thread.currentThread().getName());
+                Log.d(TAG, "onNext" + task.getDescription());
             }
 
             @Override
             public void onError(Throwable e) {
+                Log.e(TAG, "onError", e);
 
             }
 
             @Override
             public void onComplete() {
+                Log.d(TAG,"onComplete called");
 
             }
         });
